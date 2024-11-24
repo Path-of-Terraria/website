d23019c2d9eaba0ca1ce3efe648a4ad296dbe710
Collin Herber <rustaddicts@gmail.com>
2024-11-24 12:49:45 -0500

    chore: add serverts config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import Header from '$lib/components/Header.svelte';
 	import './styles.css';
 	import {SvelteToast} from "@zerodevx/svelte-toast";
+	export const ssr = false;
+	export const prerender = true;
+	export const trailingSlash = 'always';
 	let options = {
 		duration: 5000,
 		position: 'top-right',
