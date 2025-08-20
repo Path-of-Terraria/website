d5e332ae46743daaebab69f1a8ebae518a17610d
collinherber <rustaddicts@gmail.com>
2025-08-19 20:41:36 -0400

    feat: add in home page styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -19,12 +19,33 @@
     import LoginModal from "$lib/components/LoginModal.svelte";
     import {UserService} from "$lib/services/user-service";
     import SettingsModal from "$lib/components/SettingsModal.svelte";
+    import { page } from '$app/stores';
 
     let userService = new UserService();
     const isDebug = import.meta.env.VITE_DEBUG === 'true';
 
     let currentUser: IUser;
     let settingsOpen = false;
+
+    // Determine if we're on the home page
+    $: isHome = $page.url?.pathname === '/';
+
+    // Classes depending on route
+    $: navbarClass = isHome
+        ? 'bg-transparent fixed top-0 left-0 right-0 z-50 backdrop-blur-sm'
+        : 'bg-primary-100 dark:bg-primary-700';
+
+    $: brandTextClass = isHome
+        ? 'text-white'
+        : 'text-primary-700';
+
+    $: navLinkClass = isHome
+        ? 'text-white hover:text-gray-200'
+        : '';
+
+    $: hamburgerClass = isHome
+        ? 'text-white'
+        : '';
 
     const unsubscribe = user.subscribe(value => {
         // @ts-ignore
@@ -36,22 +57,22 @@
         unsubscribe();
     });
 </script>
-<Navbar class="bg-primary-100 dark:bg-primary-700">
+<Navbar class={navbarClass}>
     <NavBrand href="/">
         <img class="me-3 h-6 sm:h-9" src={logo} alt="Logo"/>
 
-        <span class="text-primary-700 self-center text-xl font-semibold whitespace-nowrap">
+        <span class={`self-center text-xl font-semibold whitespace-nowrap ${brandTextClass}`}>
 			Path of Terraria
 		</span>
     </NavBrand>
     <NavUl>
-        <NavLi href="/">Home</NavLi>
-        <NavLi href="/leaderboards">Leaderboards</NavLi>
-        <NavLi href="/trade">Trade</NavLi>
-        <NavLi href="https://wiki.pathofterraria.com" target="_blank">Wiki</NavLi>
+        <NavLi href="/" class={navLinkClass}>Home</NavLi>
+        <NavLi href="/leaderboards" class={navLinkClass}>Leaderboards</NavLi>
+        <NavLi href="/trade" class={navLinkClass}>Trade</NavLi>
+        <NavLi href="https://wiki.pathofterraria.com" target="_blank" class={navLinkClass}>Wiki</NavLi>
         {#if isDebug}
-            <NavLi href="/mod-data">Mod Data</NavLi>
-            <NavLi href="/admin">Admin</NavLi>
+            <NavLi href="/mod-data" class={navLinkClass}>Mod Data</NavLi>
+            <NavLi href="/admin" class={navLinkClass}>Admin</NavLi>
         {/if}
     </NavUl>
 
@@ -62,7 +83,7 @@
             <Avatar id="avatar-menu" src={avatar} class="cursor-pointer"/>
         {/if}
 
-        <NavHamburger class="w-full sm:flex md:w-auto md:order-1 md:hidden"/>
+        <NavHamburger class={`w-full sm:flex md:w-auto md:order-1 md:hidden ${hamburgerClass}`}/>
     </div>
     {#if currentUser}
         <Dropdown simple triggeredBy="#avatar-menu">
