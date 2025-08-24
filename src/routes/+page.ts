423858125145d17b6af680fee6e313a82d2a7ea1
collinherber <rustaddicts@gmail.com>
2025-08-24 15:24:43 -0400

    chore: ignore trailing slash

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const prerender = true;
 export const ssr = false;
-export const trailingSlash = 'always';
+export const trailingSlash = 'ignore';
 
 export const load: PageLoad = ({ params }) => {
     console.log('params', params);
