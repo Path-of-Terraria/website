ed630cf91c4ca3bf4169ef1263f9ffd0d99290d0
Collin Herber <rustaddicts@gmail.com>
2026-04-19 19:40:45 -0400

    fix: tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -6,5 +7,5 @@ export default defineConfig({
 		port: 8000,
         allowedHosts: true,
 	},
-	plugins: [sveltekit()]
+	plugins: [tailwindcss(), sveltekit()]
 });
